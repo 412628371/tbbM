a97bc987169b6967e04b979faffbe3eab9847662
clancy <xuqhwind@foxmail.com>
2017-04-24 14:46:37 +0800

    测试推送的service

--- tubobo-merchant-web/src/main/java/com/xinguang/tubobo/merchant/web/App.java
+++ tubobo-merchant-web/src/main/java/com/xinguang/tubobo/merchant/web/App.java
@@ -1,6 +1,6 @@
 package com.xinguang.tubobo.merchant.web;
 
-import com.hzmux.hzcms.common.utils.SpringContextHolder;
+import com.xinguang.tubobo.impl.merchant.service.MerchantPushService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,5 +8,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Created by Administrator on 2017/4/24.
  */
 public class App {
-    ApplicationContext context = new ClassPathXmlApplicationContext("classpath:");
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring-context.xml");
+        MerchantPushService merchantPushService = (MerchantPushService) context.getBean("merchantPushService");
+        merchantPushService.pushToUser("30126","test","订单被接单");
+    }
 }
