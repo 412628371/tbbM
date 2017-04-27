be6b6f41b58834a10c9c9c8e91b950df5dfed55a
clancy <xuqhwind@foxmail.com>
2017-04-27 15:59:02 +0800

    派单半径更改参数名

--- tubobo-merchant-impl/src/main/java/com/xinguang/tubobo/impl/merchant/common/MerchantConstants.java
+++ tubobo-merchant-impl/src/main/java/com/xinguang/tubobo/impl/merchant/common/MerchantConstants.java
@@ -6,7 +6,7 @@ package com.xinguang.tubobo.impl.merchant.common;
 public class MerchantConstants {
     public static final String DEFAULT_PAY_PASSWORD = "111111";
     public static final String TOKEN_HEADER = "Authorization";
-    public static final Double DISPATCH_RADIUS_BY_MILLS = 3000.00;
+    public static final Double DISPATCH_RADIUS_BY_KiloMILLS = 3.00;
     public static final int PAY_EXPIRED_TIME_BY_MilSECONDS = 1800000;
     public static final int GRAB_EXPIRED_TIME_BY_MilSECONDS = 300000;
 
