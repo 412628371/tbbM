ccb6d0a8bf682e32ac4438b841a1f803bd781382
xuqinghua <xuqinghua@xinguangnet.com>
2017-06-06 11:33:01 +0800

    增加异常捕获

--- tubobo-merchant-impl/src/main/java/com/xinguang/tubobo/impl/merchant/amap/RoutePlanning.java
+++ tubobo-merchant-impl/src/main/java/com/xinguang/tubobo/impl/merchant/amap/RoutePlanning.java
@@ -35,18 +35,18 @@ public class RoutePlanning {
         Double distance;
         if (LbsConstants.TRANSPORT.WALK.getCode().equals(config.getTransportType())){
             distance = gdDistanceService.getDistance(LbsConstants.TRANSPORT.WALK.getCode(),lng1,lat1,lng2,lat2);
-            logger.info("路径规划。使用高德步行距离:{}。",distance);
+            logger.info("路径规划。使用高德步行距离:{}米。",distance);
             if (distance == null){
                 distance = gdDistanceService.getDistance(LbsConstants.TRANSPORT.CAR.getCode(),lng1,lat1,lng2,lat2);
-                logger.info("路径规划，使用步行距离失败，使用高德驾车距离:{}。",distance);
+                logger.info("路径规划，使用步行距离失败，使用高德驾车距离:{}米。",distance);
             }
         }else {
             distance = gdDistanceService.getDistance(LbsConstants.TRANSPORT.CAR.getCode(),lng1,lat1,lng2,lat2);
-            logger.info("路径规划。使用高德驾车距离:{}。",distance);
+            logger.info("路径规划。使用高德驾车距离:{}米。",distance);
         }
         if (distance == null){
             distance = LocationUtil.getDistance(lat1,lng1,lat2,lng2);
-            logger.info("路径规划，获取距离失败。使用直线距离:{}。",distance);
+            logger.info("路径规划，获取距离失败。使用直线距离:{}米。",distance);
         }
         return distance;
     }
@@ -54,7 +54,7 @@ public class RoutePlanning {
     public Double getDistanceWithCar(Double lng1,Double lat1,Double lng2,Double lat2) throws MerchantClientException {
         Double distance;
         distance = gdDistanceService.getDistance(LbsConstants.TRANSPORT.CAR.getCode(),lng1,lat1,lng2,lat2);
-        logger.info("路径规划。使用高德驾车距离:{}。",distance);
+        logger.info("路径规划。使用高德驾车距离:{}米。",distance);
         if (distance == null){
             throw new MerchantClientException(EnumRespCode.FAIL);
         }
