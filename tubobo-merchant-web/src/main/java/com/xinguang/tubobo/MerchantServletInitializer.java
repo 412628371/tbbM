71b4f29deb9c521a51b09a4d5fb06231818c1119
shadeien <125417467@qq.com>
2017-10-13 14:40:56 +0800

    重载contextload的AllowBeanDefinitionOverriding为false，避免id命名重复覆盖的问题，有些类既被扫描，又在xml里面声明也会报错，已修正。

--- tubobo-merchant-web/src/main/java/com/xinguang/tubobo/MerchantServletInitializer.java
+++ tubobo-merchant-web/src/main/java/com/xinguang/tubobo/MerchantServletInitializer.java
@@ -4,8 +4,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 public class MerchantServletInitializer extends SpringBootServletInitializer {
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(com.xinguang.tubobo.Application.class);
+        return builder.sources(com.xinguang.tubobo.Application.class).initializers(new SpringApplicationContextInitializer());
     }
 }
