0d91c45b703d93444f20c55908cd6365f47277fd
闫旭 <yanx@xinguangnet.com>
2017-11-03 17:21:58 +0800

    fix longitude字段

--- tubobo-merchant-web/src/main/java/com/xinguang/tubobo/merchant/web/response/RespRiderLocation.java
+++ tubobo-merchant-web/src/main/java/com/xinguang/tubobo/merchant/web/response/RespRiderLocation.java
@@ -5,7 +5,18 @@ package com.xinguang.tubobo.merchant.web.response;
  */
 public class RespRiderLocation {
     private Double latitude;
+    //1.46 版本之前使用是该错误拼写字段 , 3个版本后 或有强制升级是去掉该字段
     private Double longtitude;
+    //1.46 版本后采用此字段
+    private Double longitude;
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
 
     public Double getLatitude() {
         return latitude;
