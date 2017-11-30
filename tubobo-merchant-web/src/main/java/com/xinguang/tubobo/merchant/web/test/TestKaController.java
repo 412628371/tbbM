fac671b8c87625d84ee2b1db3af98d69e87a7bd9
闫旭 <yanx@xinguangnet.com>
2017-11-30 18:18:32 +0800

    ka发单backdoor

--- tubobo-merchant-web/src/main/java/com/xinguang/tubobo/merchant/web/test/TestKaController.java
+++ tubobo-merchant-web/src/main/java/com/xinguang/tubobo/merchant/web/test/TestKaController.java
@@ -31,7 +31,7 @@ public class TestKaController extends MerchantBaseController<Object,Object>{
 //		rmqTakeoutAnswerProducer.sendAccepted("YZ","30126","20170001","111",null);
         MerchantOrderCreateDto dto = new MerchantOrderCreateDto();
         MerchantOrderEntity merchantOrderEntity = orderService.findByOrderNo("061711300000110");
-        dto.setUserId(userId);
+        dto.setUserId("76941");
         AddressInfoDTO consignor = new AddressInfoDTO();
         AddressInfoDTO receiver = new AddressInfoDTO();
         //插入发货方
